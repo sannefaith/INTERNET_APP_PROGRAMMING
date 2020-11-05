ad0fa30acca696ca5f49670d7553191d029b243f
sannefaith <odhiambo.faith@strathmore.edu>
2020-11-05 15:24:05 +0300

    Create db.php

---
+++ USER/db.php
@@ -0,0 +1,32 @@
+<?php
+include_once '';
+
+class Db{
+    protected $conn;
+    //constructor
+    function _construct()
+    {
+        $dsn = "mysql:host =".Utility::$dbserver.";dbname=".Utility::$dbname . "";
+        $options = [ 
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_EMULATE_PREPARES => false,
+            PDO::ATTR_DEFAULT_FETCH_MODE =>PDO::FETCH_ASSOC
+        ];
+        // instantiating the PDO class:
+        try{
+           $this->conn=new PDO($dsn,Utility::$dbuser,Utility::$dbpassword, $options);				
+         }catch (PDOException $e){
+                   echo $e->getMessage();
+         }			
+      }
+      //The connectToDB method returns the PDO connection handle:
+      public function connect_func(){
+              return $this->conn;
+      } 			
+      public function close_connect_func(){
+              $this->conn = null;
+      }
+    }
+
+}
+?>
